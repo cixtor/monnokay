80a821cffa0e543df6f7fd87568c4a9d346450dd
Cixtor <cixtords@gmail.com>
2016-07-14 09:00:00 -0500

    Added sample file with the code in the scheme preview

---
+++ _sample.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+ * tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ * quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+ * consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+ * cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+ * proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ *
+ * @package   Corporate Package
+ * @author    John Smith <john@smith.com>
+ * @copyright Copyright (C) 1997 Company Inc.
+ * @link      https://example.com/
+ * @since     Version 0.1
+ */
+
+/* --------------- */                        // Comment
+$var = "abcabcabcab";                        // String
+$var = 1234567890000;                        // Number
+$var = true && false;                        // Built-in constant
+$var = FOOBAR_FOOBAR;                        // User-defined constant
+$var = $foobar_lorem;                        // Variable
+$var = return FOOBAR;                        // Keyword              (return)
+public $var = FOOBAR;                        // Storage              (public)
+var $var = FOOBAR;                           // Storage type         (var)
+class Foobar {}                              // Class name           (Foobar)
+class Foobar extends Lorem {}                // Inherited class      (Lorem)
+function foobar() {}                         // Function name        (foobar)
+private function foobar($arg = 12) {}        // Function argument    ($arg)
+$var = str_replace("hello", 12345, null);    // Library Function     (str_replace)
+$var = this_is_custom("hello", 12345, null); // Function Name Custom (this_is_custom)
+ &&||&& >>> &&||&&                           // Invalid deprecated
+ &&||&& >>> &&||&&                           // Invalid
+?>
+<title>Datashat Remix</title>                <!-- Tag name -->
+<body attr="foobar"></body>                  <!-- Tag attribute -->
+<script>xml.documentElement.length</script>  <!-- Library constant -->
+<script>Sound.prototype.play</script>        <!-- Library class/type -->
+<script>Sound.this_is_a_variable</script>    <!-- Library variable -->
+<style type="text/css">
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 12px;
+    color: #efefef;
+}
+#container {
+    width: 600px;
+    margin: 0 auto;
+    border: 1px solid #000;
+}
+.special {
+    width: initial;
+    background: #ff0000;
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+}
+</style>
